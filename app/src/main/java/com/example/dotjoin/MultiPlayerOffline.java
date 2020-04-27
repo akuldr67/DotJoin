5c961a75edad460eba832cedfc350eeca93bc3d9
Appy <arpitbatra98@gmail.com>
2020-04-27 21:02:49 +0530

    added board image and getting size of board for any sized phone

--- app/src/main/java/com/example/dotjoin/MultiPlayerOffline.java
+++ app/src/main/java/com/example/dotjoin/MultiPlayerOffline.java
@@ -1,39 +1,68 @@
 package com.example.dotjoin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
 public class MultiPlayerOffline extends AppCompatActivity {
 
     private View view;
     private ImageView imageView;
+    private ImageView imageView2;
     private float posX,posY;
+    private int a,b,c,d;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_player_offline);
 
-        view =findViewById(R.id.view);
-        imageView=findViewById(R.id.imagee);
 
-        view.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                posX=event.getX();
-                posY=event.getY();
-                Log.d("cor","x = "+posX);
-                Log.d("cor","y = "+posY);
-                imageView.layout((int)posX,(int)posY,(int)posX+300,(int)posY+300);
-                imageView.setVisibility(View.VISIBLE);
-                return false;
+        imageView=findViewById(R.id.imagee);
+        constraintLayout=findViewById(R.id.constraint);
+
+
+
+
+
+        ViewTreeObserver vto = imageView.getViewTreeObserver();
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            public boolean onPreDraw() {
+                imageView.getViewTreeObserver().removeOnPreDrawListener(this);
+                a = imageView.getMeasuredHeight();
+                b = imageView.getMeasuredWidth();
+                c = imageView.getHeight();
+                d = imageView.getWidth();
+                int h = (c-d)/2;
+
+                imageView2=new ImageView(getApplicationContext());
+                imageView2.setImageResource(R.drawable.line);
+                imageView2.setX(100);
+                imageView2.setY(100+h);
+                imageView2.setImageAlpha(70);
+                ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(d,d);
+                constraintLayout.addView(imageView2,params);
+                return true;
             }
         });
+//        view.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                posX=event.getX();
+//                posY=event.getY();
+//                Log.d("cor","x = "+posX);
+//                Log.d("cor","y = "+posY);
+//                return false;
+//            }
+//        });
     }
 }
